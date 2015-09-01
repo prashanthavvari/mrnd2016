9728f87dab9691323b1be46bcdbc6c6e9f9e820f
prashanthavvari <apprashanth55@gmail.com>
2015-09-01 18:32:09 +0530

    for invalid characters and leap year condition

--- progrms/print_date.c
+++ progrms/print_date.c
@@ -21,7 +21,7 @@ void tens(int s)
 void ten(int s)
 {
 	int p;
-	char te[] = { "ten     twenty  thirty  fourty  fifty   sixty  seventy eighty   ninty  " };
+	char te[] = { "ten     twenty  thirty  fourty  fifty   sixty  seventy  eighty   ninty  " };
 	for (p = s * 8; p < s * 8 + 7; p++)
 		if (te[p] != ' ')
 			printf("%c", te[p]);
@@ -42,7 +42,7 @@ void mon(int s)
 void days(char c[20], int l, int k)
 {
 	int s = 0;
-	if (c[l] == '0')
+	if ((c[l] == '0')&&(c[l+1]!='0'))
 	{
 		s = (int)c[l + 1] - 49;
 		ones(s);
@@ -87,18 +87,11 @@ void dat(char c[20])
 		}
 		if (((k - l) == 2) && (p == 1))
 		{
-			if ((c[l] <= '3') && (c[l] >= '0'))
-				if ((c[l] == '3') && (c[l + 1] >= '2'))
-				 printf("invalid");
-				else
-					days(c, l, k);
-			else printf("invalid");
-				printf("-");
+			days(c, l, k);
+			printf("-");
 		}
 		if (((k - l) == 3) && (p == 2))
 		{
-			if ((c[l + 1] <= '1') && (c[l + 1] >= '0'))
-			{
 				if (c[l + 1] == '0')
 				{
 					s = (int)c[l + 2] - 49;
@@ -109,17 +102,12 @@ void dat(char c[20])
 					s = (int)c[l + 2] - 49 + 10;
 					mon(s);
 				}
-			}
-			else
-				printf("invalid");
 			printf("-");
 		}
 		else
 			xa = 1;
 		if (((k - l) == 5) && (p == 3))
 		{
-			if ((c[l + 1] >= '0') && (c[l + 1] <= '9'))
-			{
 				if (c[l + 1] != '0')
 				{
 					s = (int)c[l + 1] - 49;
@@ -133,8 +121,6 @@ void dat(char c[20])
 					printf(" hundered and ");
 				}
 				days(c, l + 3, k);
-			}
-			else printf("invalid");
 		}
 		if ((p > 2) && (k - l) != 5)
 			printf("invalid year");
@@ -142,6 +128,46 @@ void dat(char c[20])
 	}
 	if (p != 3)printf("invalid format");
 }
+int check(char s[30])
+{
+	int a[10], d=0,e,f,g,i,b=0,q=0,x=0;
+	for (i = 0; i < 8; i++)
+		a[i] = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			a[b] = (int)s[i] - 48;
+			b++;
+		}
+		else
+			x = x + 1;
+	}
+	e = a[0]*10 + a[1];
+	f = a[2]*10 + a[3];
+	g = a[4] * 1000 + a[5] * 100 + a[6] * 10 + a[7];
+	switch (f)
+	{
+	case 1:if (e > 31) s = 1; break;	// e gives date f gives month g gives year
+	case 2:if (e > 28) s = 1;; break;
+	case 3:if (e > 31) s = 1; break;
+	case 4:if (e > 30) s = 1; break;
+	case 5:if (e > 31) s = 1; break;
+	case 6:if (e > 30) s = 1; break;
+	case 7:if (e > 31) s = 1; break;
+	case 8:if (e > 31)s = 1; break;
+	case 9:if (e > 30)s = 1; break;
+	case 10:if (e > 31) s = 1; break;
+	case 11:if (e > 30) s = 1; break;
+	case 12:if (e > 31) s = 1; break;
+	default:s = 1;
+	};
+ if ((((g % 4 == 0) && (g % 100 != 0)) || (g % 400 == 0)) && (e == 29)) s = 0;
+	if ((g != 0) && (s != 1)&&(x<=2)&&(e!=0)&&(f!=0))
+		return(0);
+	else
+		return(1);
+	}
 void main()
 {
 	char c[20];
@@ -149,29 +175,35 @@ void main()
 	printf("format\n");
 	printf("DD-MM-YYYY\n");
 	gets(c);
-	for (i = 0; c[i] != '\0'; i++)
+	p=check(c);
+	if (p == 1)
+		printf("\ninvalid");
+	else
 	{
-		if (c[i] == '-')
-		{
+			for (i = 0; c[i] != '\0'; i++)
+			{
+			if (c[i] == '-')
+			{
 			k = i;
 			t = t+1;
 			p = l;
-		}
-		if ((!(k - p == 2))&&(t==1))
-		{
+			}
+			if ((!(k - p == 2))&&(t==1))
+			{
 			printf("invalid");
 			s = 1;
 			break;
-		}
-		if (!(k - p == 3) && (t == 2))
-		{
+			}
+			if (!(k - p == 3) && (t == 2))
+			{
 			printf("invalid");
 			s = 2;
 			break;
-		}
-		l = k;
+			}
+			l = k;
+			}
+			if (s==0)
+			dat(c);
 	}
-	if (s==0)
-	dat(c);
 	getch();
 }
