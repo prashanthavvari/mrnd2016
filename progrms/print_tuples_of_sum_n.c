ceb49e511da2d31c763626d524ccc48651c380fb
prashanthavvari <apprashanth55@gmail.com>
2015-09-02 04:26:33 +0530

    Update print_tuples_of_sum_n.c

--- progrms/print_tuples_of_sum_n.c
+++ progrms/print_tuples_of_sum_n.c
@@ -30,37 +30,58 @@ quicksort(x,j+1,last);
 }
 void add(int a[30], int n)
 {
-	int i, j,lb,ub,mid,num,x=0;
+	int i, j, lb, ub, mid, num, x = 0, y,c=0;
 	lb = 1;
 	ub = n;
+	y = n;
 	scanf("%d", &num);
-	for (i = 1; i <= n; i++)
+	printf("the tuples are");
+	if (n == 1)
 	{
-		j = i + 1;
-		while (j<=n)
+		printf("%d", a[1]); c = 1;
+	}
+	if (n > 1)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			y = ub;
+			j = i + 1;
+			if (a[i] == num&&a[i + 1] != num){
+				printf("%d\n", a[i]);
+				c = 1;
+			}
+			while (j <= n)
 			{
-				if (ub>0)
-				mid = (j + ub) / 2;
+				if (y > 0)
+					mid = (j + y) / 2;
+
 				if (a[mid] + a[i] == num)
 				{
-					if((a[i] != a[i-1]))
-					printf("%d,%d\n", a[i],a[mid]);
+					if ((a[i] != a[i - 1]))
+						printf("%d,%d\n", a[i], a[mid]);
+					c = 1;
 					break;
 				}
 				else if (a[mid] + a[i] < num)
+				{
 					j = mid + 1;
+				}
 				else if (a[mid] + a[i] > num)
 				{
-					ub = mid - 1;
+					y = mid - 1;
 					x = x + 1;
 				}
-				if (x>n)
+				if (x > n)
 					break;
+				if (y < j) break;
 			}
-			if (mid <= 0||ub<=0)
+			if (mid <= 0 || ub <= 0)
 				break;
 		}
 	}
+	if (c == 0)
+		printf(" not found");
+}
 void main()
 {
 	int a[30], n,i;
@@ -70,7 +91,5 @@ void main()
 	scanf("%d", &a[i]);
 	quicksort(a,0,n);
 	add(a, n);
-	/*for (i = 1; i <=n; i++)
-		printf("%d", a[i]);*/
 	getch();
 }
